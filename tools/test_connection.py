56cd72a5b5d228fabfe0cf01df892de00a3d04ec
Edoardo Arnaudo <edoardo.arnaudo@linksfoundation.com>
2025-02-03 10:10:21 +0100

    Improve example script with read/write

--- tools/test_connection.py
+++ tools/test_connection.py
@@ -7,14 +7,28 @@ import pika
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Connect to RabbitMQ server using TLS")
-    parser.add_argument("--username", default="admin", help="Username for RabbitMQ authentication")
-    parser.add_argument("--password", default="admin", help="Password for RabbitMQ authentication")
-    parser.add_argument("--server-host", default="localhost", help="URL of RabbitMQ server")
+    parser.add_argument(
+        "--username", default="admin", help="Username for RabbitMQ authentication"
+    )
+    parser.add_argument(
+        "--password", default="admin", help="Password for RabbitMQ authentication"
+    )
+    parser.add_argument(
+        "--server-host", default="localhost", help="URL of RabbitMQ server"
+    )
     parser.add_argument("--server-port", default=5671, help="Port of RabbitMQ server")
-    parser.add_argument("--server-vhost", default="/", help="Virtual host of RabbitMQ server")
-    parser.add_argument("--ca-cert-file", "-CA", required=True, help="File to the CA certificate file")
-    parser.add_argument("--cert-file", "-C", required=True, help="File to the client certificate file")
-    parser.add_argument("--key-file", "-K", required=True, help="File to the client key file")
+    parser.add_argument(
+        "--server-vhost", default="/", help="Virtual host of RabbitMQ server"
+    )
+    parser.add_argument(
+        "--ca-cert-file", "-CA", required=True, help="File to the CA certificate file"
+    )
+    parser.add_argument(
+        "--cert-file", "-C", required=True, help="File to the client certificate file"
+    )
+    parser.add_argument(
+        "--key-file", "-K", required=True, help="File to the client key file"
+    )
     return parser.parse_args()
 
 
@@ -51,14 +65,37 @@ def main():
         )
         channel = connection.channel()
 
-        # Example usage: Declare a queue and publish a message
-        channel.queue_declare(queue="hello")
-        channel.basic_publish(exchange="", routing_key="hello", body="Hello, RabbitMQ!")
-        print(" [x] Sent 'Hello, RabbitMQ!'")
+        # Example usage: Declare a queue and bind "hello.something" messages to it
+        channel.queue_declare(queue="q.hello")
+        channel.queue_bind(exchange="amq.topic", queue="q.hello", routing_key="hello.#")
+        # Now publish a message to the same exchange: the exchange will take care of
+        # delivering the payload to every queue that matches the routing key
+        channel.basic_publish(
+            exchange="amq.topic", routing_key="hello", body="Hello, RabbitMQ!"
+        )
+        print("[Publisher] Sent 'Hello, RabbitMQ!'")
 
+        def callback(ch, method, properties, body):
+            # We expect to receive a single message,
+            # after that we can stop consuming to continue the execution and exit
+            print(f"[Consumer] Received message '{method.routing_key}': {body}")
+            channel.stop_consuming()
+
+        # declare how to handle messages, in this case we read from q.hello
+        # and we handle messages using the `callback` function
+        channel.basic_consume(
+            queue="q.hello", on_message_callback=callback, auto_ack=True
+        )
+        # We can now start consuming, note: this is blocking!
+        channel.start_consuming()
+
+    finally:
+        # the queue does not need to be created and destroyed each time
+        # in a production setting, the queues will be fixed and persistent
+        channel.queue_delete(queue="q.hello")
+        # last, we close the channel and connection to free the resources
+        channel.close()
         connection.close()
-    except Exception as e:
-        print("Error:", str(e))
 
 
 if __name__ == "__main__":
